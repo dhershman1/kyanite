57f19ab973e441155e97b12f78fda0d9b3e4597b
dhershman1 <dustinh17@gmail.com>
2018-06-12 17:12:31 -0400

    Added ascendBy function

---
+++ src/ascendBy.js
@@ -0,0 +1,36 @@
+import curry from './curry'
+import ascend from './ascend'
+
+/**
+ * @name ascendBy
+ * @since v0.2.1
+ * @category Array
+ * @sig Function -> Any -> Any -> Number
+ * @description Can be used with sort to ascend an array based on the function passed in
+ * @param {Function} fn The function to use on values within our array
+ * @param {Any} a The first value to compare
+ * @param {Any} b The second value to compare
+ * @return {Number} A number based on where it falls when compared with the other value
+ *
+ * @example
+ * [
+ *  { name: 'bob' },
+ *  { name: 'amanda' },
+ *  { name: 'carl' },
+ *  { name: 'amanda' }
+ * ].sort(ascendBy(x => x.name)) // => [{ name: 'amanda' }, { name: 'amanda' }, { name: 'bob' }, { name: 'carl' }]
+ *
+ * // It's also curried
+ *
+ * const desc = ascendBy(x => x.name)
+ *
+ * [
+ *  { name: 'bob' },
+ *  { name: 'amanda' },
+ *  { name: 'carl' },
+ *  { name: 'amanda' }
+ * ].sort(desc) // => [{ name: 'amanda' }, { name: 'amanda' }, { name: 'bob' }, { name: 'carl' }]
+ */
+const ascendBy = (fn, a, b) => ascend(fn(a), fn(b))
+
+export default curry(ascendBy)
