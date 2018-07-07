2decaa43da122c9d7f53b782f375a65266b471d5
dhershman1 <dustinh17@gmail.com>
2018-07-07 13:07:56 -0400

    Add some extra testing to pipe

--- tests/function/pipe.js
+++ tests/function/pipe.js
@@ -14,3 +14,12 @@ test('Is curried', t => {
   t.is(piper(10), 24)
   t.end()
 })
+
+test('Handles customized functions', t => {
+  const custom = pipe([x => x + 1, x => x * 2])
+
+  t.same([1, 2, 3].map(custom), [4, 6, 8])
+  t.same([3, 4, 5].map(custom), [8, 10, 12])
+  t.same([10, 11, 12].map(custom), [22, 24, 26])
+  t.end()
+})
