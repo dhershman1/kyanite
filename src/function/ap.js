446a376931d4868b75be9c5543313146937e7c30
dhershman1 <dustinh17@gmail.com>
2018-07-07 12:51:08 -0400

    better description for the ap function

--- src/function/ap.js
+++ src/function/ap.js
@@ -6,10 +6,10 @@ import curry from './curry'
  * @category Function
  * @sig Array (a -> b) -> Array a -> Array b
  * @description
- * Applies a list of functions over an array of data
+ * Takes an array of functions to be applied to an array of data, concating the results together
  * @param {Array} fns The list of functions to apply
  * @param {Array} list The array of data to run the functions on
- * @return {Array} A new array of data modified by the functions
+ * @return {Array} A new array of data modified by the functions concated together
  * @example
  * ap([x => x + 1, x => x * 2], [1, 2, 3]) // => [2, 3, 4, 2, 4, 6]
  *
