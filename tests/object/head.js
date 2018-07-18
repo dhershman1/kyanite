0a3c76af396e7d82d42b480da3112d3489282a6c
dhershman1 <dustinh17@gmail.com>
2018-07-18 16:14:43 -0400

    Added Head function

---
+++ tests/object/head.js
@@ -0,0 +1,16 @@
+import head from '../../src/object/head'
+import test from 'tape'
+
+test('Returns the first value of an object (simple)', t => {
+  const result = head({ a: 1, b: 2, c: 3 })
+
+  t.same(result, 1)
+  t.end()
+})
+
+test('Returns the first value complex data', t => {
+  const result = head({ a: { b: 2, c: 3 }, d: 4 })
+
+  t.same(result, { b: 2, c: 3 })
+  t.end()
+})
