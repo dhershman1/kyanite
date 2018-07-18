35c8d4923a3e2e3ea86fa3d412f6953f264bfeff
dhershman1 <dustinh17@gmail.com>
2018-07-18 16:44:35 -0400

    Tweaked max comparison

--- src/array/max.js
+++ src/array/max.js
@@ -4,14 +4,13 @@
  * @category Array
  * @description
  * Goes through an array of values and grabs the last value of the array when it's been sorted
- * @param {Array} list The Array to sort and grab from
- * @return {Any} Returns the item at the end of an array based on what's passed in
+ * @param {Array} list The Array search through
+ * @return {Any} The item that was deemed to be the max
  *
  * @example
  * max([1, 3, 2, 5, 4]); // => 5
  * max(['c', 'a', 'b', 'f']); // => 'f'
  */
-const max = list =>
-  list.reduce((a, b) => a >= b ? a : b)
+const max = list => list.reduce((a, b) => a >= b ? a : b)
 
 export default max
