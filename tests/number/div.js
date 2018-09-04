ea4e630807ed5d7aa64f3d02eef53d9d3bdd91e0
dhershman1 <dustinh17@gmail.com>
2018-09-04 11:57:03 -0400

    Swapped div params

--- tests/number/div.js
+++ tests/number/div.js
@@ -7,19 +7,20 @@ test('Divides the numbers', t => {
 })
 
 test('Converts string and multiplies anyway', t => {
-  t.is(div(1, '2'), 0.5)
+  t.is(div('2', 1), 0.5)
   t.end()
 })
 
 test('Converts number to negative if a passed value is negative', t => {
-  t.is(div(3, -1), -3)
+  t.is(div(-1, 3), -3)
   t.end()
 })
 
 test('Is curried', t => {
-  const divide = div(15)
+  const d = div(3)
+  const d2 = div('5')
 
-  t.is(divide(3), 5)
-  t.is(divide('5'), 3)
+  t.is(d(15), 5)
+  t.is(d2(15), 3)
   t.end()
 })
