0d11da93210f8f5d44cf5bdfabd635cbadf1c31a
dhershman1 <dustinh17@gmail.com>
2018-09-04 13:34:10 -0400

    Added toUpper function

---
+++ tests/string/toUpper.js
@@ -0,0 +1,8 @@
+import test from 'tape'
+import toUpper from '../../src/string/toUpper'
+
+test('Transforms provided string', t => {
+  t.is(toUpper('hi'), 'HI')
+  t.is(toUpper('test.123.hello'), 'TEST.123.HELLO')
+  t.end()
+})
