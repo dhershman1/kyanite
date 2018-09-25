c5b1abe8fdd5d1ccf3cd3e19522e907f2f5a3138
dhershman1 <dustinh17@gmail.com>
2018-09-25 14:34:56 -0400

    Switched up includes documentation

--- src/array/includes.js
+++ src/array/includes.js
@@ -7,10 +7,9 @@ import curry from '../function/curry'
  * @sig a → Array a → Boolean
  * @description
  * Checks to see if the provided list contains at at least 1 of the provided value within it
- * @param {Any} a The value we want to search the list for
+ * @param {Any} value The value we want to search the list for
  * @param {Array} list The list we want to search through
  * @return {Boolean} A Boolean based on if the value is found or not
- *
  * @example
  * includes(3, [1, 2, 3]) // => true
  *
@@ -20,7 +19,7 @@ import curry from '../function/curry'
  *
  * checker([1, 2, 3]) // => true
  */
-const includes = (a, list) =>
-  list.indexOf(a) !== -1
+const includes = (value, list) =>
+  list.indexOf(value) !== -1
 
 export default curry(includes)
