65d8646301dd98f7681f5eef06346874f90084f5
dhershman1 <dustinh17@gmail.com>
2018-09-26 14:15:21 -0400

    fix when documentation

--- src/function/when.js
+++ src/function/when.js
@@ -5,21 +5,21 @@ import curryN from './curryN'
  * @since v0.1.0
  * @category Function
  * @sig (a -> Boolean) -> (a -> b) -> a -> b
- * @description Takes a value and if it passes the check function (1st param) then it will apply the action function (2nd param) otherwise undefined is given back
+ * @description Takes a value and if it passes the check function (1st param) then it will apply the action function (2nd param) otherwise it gives back the given value
  * @param {Function} fn The check function that when passed if returns a truthy value trigger the action
  * @param {Function} act The action function which is fired when the logic passes
  * @param {Any} x The argument to pass to both functions
- * @return {Any} Returns whatever the action function returns, or undefined
+ * @return {Any} Returns whatever the action function returns, or the value that was passed in
  * @example
  * when(x => x > 2, x => x * 2, 5) // => 10
- * when(x => x > 5, x => x * 2, 5) // => undefined
+ * when(x => x > 5, x => x * 2, 5) // => 5
  *
  * // It's also curried
  *
  * const w = when(x => x > 2, x => x * 2)
  *
  * w(5) // => 10
- * w(1) // => undefined
+ * w(1) // => 1
  */
 const when = (fn, act, x) =>
   fn(x) ? act(x) : x
