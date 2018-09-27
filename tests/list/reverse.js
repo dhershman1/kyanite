c73234549c30c529eb1b3a227d571c115eabe050
dhershman1 <dustinh17@gmail.com>
2018-09-27 17:52:58 -0400

    Moved reverse to List types

--- tests/list/reverse.js
+++ tests/list/reverse.js
@@ -1,20 +1,22 @@
-import reverse from '../../src/array/reverse'
+import reverse from '../../src/list/reverse'
 import test from 'tape'
 
 test('reverse -- Returns reversed array', t => {
-  t.deepEqual(reverse([1, 2, 3]), [3, 2, 1])
+  t.same(reverse([1, 2, 3]), [3, 2, 1])
+  t.same(reverse('abc'), 'cba')
   t.end()
 })
 
 test('reverse -- Returns empty array when given an empty array', t => {
-  t.deepEqual(reverse([]), [])
+  t.same(reverse([]), [])
+  t.same(reverse(''), '')
   t.end()
 })
 
 test('reverse -- Does not mutate original array', t => {
   const x = [1, 2, 3]
 
-  t.deepEqual(reverse(x), [3, 2, 1])
-  t.deepEqual(x, x)
+  t.same(reverse(x), [3, 2, 1])
+  t.same(x, x)
   t.end()
 })
