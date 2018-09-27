30d9b68e36f2348074c38e6ae2c7b6eee7c38b04
dhershman1 <dustinh17@gmail.com>
2018-09-27 16:30:19 -0400

    Fixed when doc typos

--- src/function/when.js
+++ src/function/when.js
@@ -6,7 +6,7 @@ import curryN from './curryN'
  * @category Function
  * @sig (a -> Boolean) -> (a -> b) -> a -> b
  * @description Takes a value and if it passes the check function (1st param) then it will apply the action function (2nd param) otherwise it gives back the given value
- * @param {Function} fn The check function that when passed if returns a truthy value trigger the action
+ * @param {Function} fn The check function that when passed triggers the action function
  * @param {Function} act The action function which is fired when the logic passes
  * @param {Any} x The argument to pass to both functions
  * @return {Any} Returns whatever the action function returns, or the value that was passed in
