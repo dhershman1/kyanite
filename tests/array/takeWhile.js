b1501cc74bd42bc7fd8f33849c7d50ab4363bd4a
dhershman1 <dustinh17@gmail.com>
2018-09-28 11:34:03 -0400

    Added new takeWhile function

---
+++ tests/array/takeWhile.js
@@ -0,0 +1,16 @@
+import takeWhile from '../../src/array/takeWhile'
+import test from 'tape'
+
+test('takeWhile -- Basic usage', t => {
+  t.same(takeWhile(x => x < 4, [1, 2, 3, 4, 5, 6]), [1, 2, 3])
+  t.same(takeWhile(x => x % 2 !== 0, [1, 3, 5]), [1, 3, 5])
+  t.end()
+})
+
+test('takeWhile -- Is curried', t => {
+  const fn = takeWhile(x => x < 4)
+
+  t.same(fn([1, 2, 3, 4, 5]), [1, 2, 3])
+  t.same(fn([3, 4, 5]), [3])
+  t.end()
+})
