b99f0c1ef44d75a8a5c65a6e51ae27ef4cf1d0e6
dhershman1 <dustinh17@gmail.com>
2018-10-10 14:29:24 -0400

    Re wrote internal scripts for async usage

--- scripts/create-export.js
+++ scripts/create-export.js
@@ -2,32 +2,20 @@ const fs = require('fs')
 const path = require('path')
 const globby = require('globby')
 
-const listFns = type => {
-  const fileList = type
-    ? [`src/${type}/*.js`, `!src/${type}/index.js`]
-    : ['src/**/*.js', '!src/**/index.js', '!src/_internals']
+globby(['src/**/*.js', '!src/index.js', '!src/_internals'])
+  .then(files =>
+    files.map(f => {
+      const { dir, base, name } = path.parse(f)
 
-  return globby.sync(fileList)
-    .map(file => {
-      const { dir, base, name } = path.parse(file)
+      return `export { default as ${name} } from './${dir.replace('src/', '')}/${base}'`
+    }))
+  .then(list => fs.writeFile('src/index.js', `${list.join('\n')}\n`, err => {
+    if (err) {
+      throw err
+    }
 
-      return {
-        name,
-        fullPath: !type
-          ? `./${dir.replace('src/', '')}/${base}`
-          : `./${base}`
-      }
-    })
-}
-
-const types = ['array', 'function', 'list', 'number', 'object', 'string', '']
-
-types.forEach(v => {
-  const propertyRequireLines = listFns(v)
-    .map(fn => `export { default as ${fn.name} } from '${fn.fullPath}'`)
-  const indexLines = []
-    .concat(propertyRequireLines.join('\n'))
-    .join('\n')
-
-  fs.writeFileSync(v ? `src/${v}/index.js` : 'src/index.js', `${indexLines}\n`)
-})
+    console.log('Finished Writing index.js...')
+  }))
+  .catch(err => {
+    throw err
+  })
