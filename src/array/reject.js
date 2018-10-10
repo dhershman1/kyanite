552be6c5037ec1bc57daaff6b4cdc8a396ee0637
dhershman1 <dustinh17@gmail.com>
2018-10-10 17:09:29 -0400

    Fix up rejects documentation

--- src/array/reject.js
+++ src/array/reject.js
@@ -3,18 +3,19 @@ import curry from '../function/curry'
 /**
  * @name reject
  * @since v0.1.0
- * @category Array
- * @sig Filterable f => (a → Boolean) → f a → f a
+ * @category Function
+ * @sig (a -> Boolean) -> Array [a] -> Array [a]
  * @description
- * Filter through a filterable data piece using the provided function returns only those that fail the function test
+ * Iterate through a list and reject any value that does not pass the provided function
  * @param {Function} fn The predicate function to run on our values
  * @param {Array} list The filterable list to go through
- * @return {Array} Returns a new Array or Object based on the type of list provided
+ * @return {Array} Returns a new Array of values that were not rejected
  *
  * @example
  * const isEven = n => n % 2 === 0
  *
  * reject(isEven, [1, 2, 3, 4]) // => [1, 3]
+ * reject(x => x.val > 2, [{ val: 2 }, { val: 5 }, { val: 3 }]) // => [{ val: 2 }]
  *
  * // Is also curried
  *
