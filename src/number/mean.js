e931a9f850904e713fea41b41fba430a92051906
dhershman1 <dustinh17@gmail.com>
2018-10-15 14:45:09 -0400

    cleaned up mean and changed its return slightly

--- src/number/mean.js
+++ src/number/mean.js
@@ -1,4 +1,7 @@
 import add from './add'
+import divide from './divide'
+import length from '../list/length'
+import reduce from '../array/reduce'
 
 /**
  * @name mean
@@ -7,19 +10,14 @@ import add from './add'
  * @sig [Number] -> Number
  * @description Gets the average from a given array of numbers
  * @param  {Array} x An array of numbers to add together
- * @return {Number} Returns the mean avg of the numbers
+ * @return {Number} Returns the mean average of the numbers
  *
  * @example
  * mean([1, 2, 3, 2]) // => 2
- * mean([]) // => 0
- * mean() // => 0
+ * mean([2]) // => 2
+ * mean([]) // => NaN
+ * mean() // => Ref Error
  */
-const mean = (x = []) => {
-  if (!x.length) {
-    return 0
-  }
-
-  return x.reduce(add, 0) / x.length
-}
+const mean = x => divide(length(x), reduce(add, 0, x))
 
 export default mean
