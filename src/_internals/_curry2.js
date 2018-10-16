76eee4e026a0182afc91e9e91dd609723c30e1cf
dhershman1 <dustinh17@gmail.com>
2018-10-16 13:38:06 -0400

    tweak curry2

--- src/_internals/_curry2.js
+++ src/_internals/_curry2.js
@@ -1,5 +1,5 @@
 
-const _curry2 = function _curry2 (fn) {
+function _curry2 (fn) {
   return function f2 (a, b) {
     if (!arguments.length) {
       return f2
@@ -9,10 +9,6 @@ const _curry2 = function _curry2 (fn) {
       return function (_b) {
         return fn(a, _b)
       }
-    }
-
-    if (arguments.length === 2) {
-      return fn(a, b)
     }
 
     return fn(a, b)
