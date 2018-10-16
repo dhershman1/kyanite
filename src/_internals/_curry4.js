2c8a7f44c3765a513f22399a08c4fb8476811e41
dhershman1 <dustinh17@gmail.com>
2018-10-16 15:28:11 -0400

    flip curry4 cases

--- src/_internals/_curry4.js
+++ src/_internals/_curry4.js
@@ -6,15 +6,17 @@ import _curry3 from './_curry3'
  */
 function _curry4 (fn) {
   return function f4 (a, b, c, d) {
+    console.log(arguments.length)
+
     switch (arguments.length) {
       case 0:
         return f4
       case 1:
-        return _curry2(function (_b, _c, _d) {
+        return _curry3(function (_b, _c, _d) {
           return fn(a, _b, _c, _d)
         })
       case 2:
-        return _curry3(function (_c, _d) {
+        return _curry2(function (_c, _d) {
           return fn(a, b, _c, _d)
         })
       case 3:
