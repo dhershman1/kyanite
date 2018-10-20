50d4add125b31c755cbe5ed199278578f1ac4642
dhershman1 <dustinh17@gmail.com>
2018-10-19 21:57:46 -0400

    fixed factors and range unit tests

--- tests/number/factors.js
+++ tests/number/factors.js
@@ -9,12 +9,3 @@ test('factors -- Find the factors of the provided numbers', t => {
   t.same(factors(), [])
   t.end()
 })
-
-test('factors -- Should throw an error when values are NaN', t => {
-  try {
-    factors('h')
-  } catch (err) {
-    t.is(err.message, 'Arguments should be Numbers')
-    t.end()
-  }
-})
