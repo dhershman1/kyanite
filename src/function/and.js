2fb40875b4c9cf26a6aa0f8148b01cc7efd6f6c4
dhershman1 <dustinh17@gmail.com>
2018-10-23 13:02:39 -0400

    Fixed docs for and/or

--- src/function/and.js
+++ src/function/and.js
@@ -7,7 +7,8 @@ import _curry2 from '../_internals/_curry2'
  * @sig Boolean -> Boolean -> Boolean
  * @description
  * Runs an and comparison on the two values passed in
- * @param {...Boolean} args The values to be used to make sure all of them are passing
+ * @param {Boolean} a The first value to compare
+ * @param {Boolean} b The second value to compare
  * @return {Boolean} The evaluated outcome of the parameters
  *
  * @example
