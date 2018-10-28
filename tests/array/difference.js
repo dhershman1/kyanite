e92dc548c5640f5f3547124f1f5d96da5b279e90
dhershman1 <dustinh17@gmail.com>
2018-10-27 20:57:28 -0400

    expanded difference unit tests

--- tests/array/difference.js
+++ tests/array/difference.js
@@ -3,6 +3,7 @@ import test from 'tape'
 
 test('difference -- Returns the difference in arrays', t => {
   t.same(difference([[1, 2, 3], [1]]), [2, 3])
+  t.same(difference([[1], [1, 2, 3]]), [2, 3])
   t.same(difference([['foo', 'bar', 'baz', 'oops', 'charlie'], ['oops', 'baz', 'bobby']]), ['foo', 'bar', 'charlie', 'bobby'])
   t.same(difference([[1, 2, 3], [4, 5, 6]]), [1, 2, 3, 4, 5, 6])
   t.end()
