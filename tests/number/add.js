ea0f564fb63b59e4d7c18e946d3cf79cd94fdb76
dhershman1 <dustinh17@gmail.com>
2018-11-05 11:47:46 -0500

    Fix add unit tests

--- tests/number/add.js
+++ tests/number/add.js
@@ -7,14 +7,9 @@ test('add -- Adds two numbers together', t => {
   t.end()
 })
 
-test('add -- Handles if input is a string', t => {
-  t.is(add(1, '2'), 3)
-
-  t.end()
-})
-
-test('add -- Handles if both inputs are strings', t => {
-  t.is(add('1', '10'), 11)
+test('add -- Acts like a normal concat if value is a string', t => {
+  t.is(add(1, '2'), '12')
+  t.is(add('1', '10'), '110')
 
   t.end()
 })
@@ -23,7 +18,5 @@ test('add -- Is curried', t => {
   const adder = add(3)
 
   t.is(adder(4), 7)
-  t.is(adder('4'), 7)
-
   t.end()
 })
