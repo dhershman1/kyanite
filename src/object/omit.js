5c3bd6b105633019df842f43ba57d27192a597a5
dhershman1 <dustinh17@gmail.com>
2018-11-06 16:46:43 -0500

    Fix omits documentation

--- src/object/omit.js
+++ src/object/omit.js
@@ -7,17 +7,17 @@ import _assocǃ from '../_internals/_assocǃ'
  * @category Object
  * @sig [String] -> {String: *} → {String: *}
  * @description Create a new Array/Object by omitting the requested values
- * @param  {Array} key The key(s) in which to omit from the data
+ * @param  {Array} keys The keys in which to omit from the data
  * @param  {Object} x The object to search through and filter
  * @return {Object} Returns the newly created data without the omitted values
  *
  * @example
- * const obj = omit('test', { test: '3432', thing: 123 }) // => { thing: 123 }
+ * const obj = omit(['test'], { test: '3432', thing: 123 }) // => { thing: 123 }
  * const arr = omit(['a', 'b'], { a: 1, b: 2, c: 3}) // => { c: 3 }
  *
  * // omit is curried
  *
- * const omitKeys = omit('test')
+ * const omitKeys = omit(['test'])
  *
  * omitKeys({ test: '3432', thing: 123 }) // => { thing: 123 }
  */
