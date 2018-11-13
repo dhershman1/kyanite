8d867bcc2d722c30b858607a94565138c65d61ad
dhershman1 <dustinh17@gmail.com>
2018-11-13 12:32:23 -0500

    Created new common.js file for fallbacks

--- dist/kyanite.js
+++ dist/kyanite.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (factory((global.kyanite = {})));
+  (factory((global.K = {})));
 }(this, (function (exports) { 'use strict';
 
   function _curry2(fn) {
