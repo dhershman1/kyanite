92798e717504daf3281a1972cc3bdcfe1abc33bd
dhershman1 <dustinh17@gmail.com>
2018-11-13 15:41:34 -0500

    Pointing the package.json to the right spot

--- dist/kyanite.js
+++ dist/kyanite.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (factory((global.K = {})));
+  (factory((global.kyanite = {})));
 }(this, (function (exports) { 'use strict';
 
   function _curry2(fn) {
