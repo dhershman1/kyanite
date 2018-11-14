2a5c0d7b0a78cb8d62ac115544774e03ba28a64c
dhershman1 <dustinh17@gmail.com>
2018-11-14 13:09:35 -0500

    Improved handling of documentation and generation

--- docs/scripts/search.js
+++ docs/scripts/search.js
@@ -1,42 +1,45 @@
-$( document ).ready(function() {
-    jQuery.expr[':'].Contains = function(a,i,m){
-        return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase())>=0;
-    };
-    //on search
-    $("#nav-search").on("keyup", function(event) {
-        var search = $(this).val();
-        if (!search) {
-            //no search, show all results
-            $("nav > ul > li").show();
-            
-            if(typeof hideAllButCurrent === "function"){
-                //let's do what ever collapse wants to do
-                hideAllButCurrent();
-            }
-            else{
-                //menu by default should be opened
-                $("nav > ul > li > ul li").show();
-            }
-        }
-        else{
-            //we are searching
-            //show all parents
-            $("nav > ul > li").show();
-            //hide all results
-            $("nav > ul > li > ul li").hide();
-            //show results matching filter
-            $("nav > ul > li > ul").find("a:Contains("+search+")").parent().show();
-            //hide parents without children
-            $("nav > ul > li").each(function(){
-                if($(this).find("a:Contains("+search+")").length == 0 && $(this).children("ul").length === 0){
-                    //has no child at all and does not contain text
-                    $(this).hide();
-                }
-                else if($(this).find("a:Contains("+search+")").length == 0 && $(this).find("ul").children(':visible').length == 0){
-                    //has no visible child and does not contain text
-                    $(this).hide();
-                }
-            });
-        }
-    });
-});
+const search = document.getElementById('nav-search')
+const navItems = Array.from(document.querySelectorAll('.nav-item'))
+
+/* eslint-disable no-labels */
+
+const fuzzySearch = (needle, haystack) => {
+  const hLen = haystack.length
+  const nLen = needle.length
+  let j = 0
+
+  if (nLen > hLen) {
+    return false
+  }
+
+  if (nLen === hLen) {
+    return needle === haystack
+  }
+
+  outer: for (let i = 0; i < nLen; i++) {
+    const nChar = needle.charCodeAt(i)
+
+    for (j; j < hLen; j++) {
+      if (haystack.charCodeAt(j) === nChar) {
+        continue outer
+      }
+    }
+
+    return false
+  }
+
+  return true
+}
+
+search.oninput = ({ target }) => {
+  const val = target.value.toLowerCase()
+
+  navItems.forEach(el => {
+    if (fuzzySearch(val, el.innerText.toLowerCase()) ||
+            fuzzySearch(val, el.getAttribute('data-cat').toLowerCase())) {
+      el.classList.remove('hidden')
+    } else {
+      el.classList.add('hidden')
+    }
+  })
+}
