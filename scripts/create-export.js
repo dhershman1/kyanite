81275393399f83f078d545ccbdac5228e8bec7d4
dhershman1 <dustinh17@gmail.com>
2018-11-16 16:08:03 -0500

    Cleaned up cjs

--- scripts/create-export.js
+++ scripts/create-export.js
@@ -3,8 +3,8 @@ const path = require('path')
 const globby = require('globby')
 
 const files = globby.sync(['src/**/*.js', '!src/index.js', '!src/common.js', '!src/_internals'])
-// Build Exports
 
+// Build Exports
 fs.writeFileSync('src/index.js', `${files.map(f => {
   const { dir, base, name } = path.parse(f)
 
@@ -12,13 +12,3 @@ fs.writeFileSync('src/index.js', `${files.map(f => {
 }).join('\n')}\n`)
 
 console.log('Finished Writing Exports...')
-
-// Build CJS
-fs.writeFileSync('src/common.js', `module.exports = {}
-${files.map(f => {
-    const { dir, base, name } = path.parse(f)
-
-    return `module.exports.${name} = require('./${dir.replace('src/', '')}/${base}')`
-  }).join('\n')}\n`)
-
-console.log('Finished Writing CJS...')
