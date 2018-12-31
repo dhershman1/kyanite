f9f6bffa2dd9672837776bee410de5d566cfcbe4
dhershman1 <dustinh17@gmail.com>
2018-12-31 13:36:52 -0500

    Tweak exports script

--- scripts/create-export.js
+++ scripts/create-export.js
@@ -3,12 +3,15 @@ const path = require('path')
 const globby = require('globby')
 
 const files = globby.sync(['src/**/*.js', '!src/index.js', '!src/common.js', '!src/_internals'])
+const buildRes = n =>
+  files.map(f => {
+    const { dir, base, name } = path.parse(f)
+
+    return `export { default as ${name} } from '${n}/${dir.replace('src/', '')}/${base}'`
+  }).join('\n')
 
 // Build Exports
-fs.writeFileSync('src/index.js', `${files.map(f => {
-  const { dir, base, name } = path.parse(f)
-
-  return `export { default as ${name} } from './${dir.replace('src/', '')}/${base}'`
-}).join('\n')}\n`)
+fs.writeFileSync('src/index.js', `${buildRes('.')}\n`)
+// fs.writeFileSync('dist/index.mjs', `${buildRes('../src')}\n`)
 
 console.log('Finished Writing Exports...')
