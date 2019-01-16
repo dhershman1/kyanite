3833e0af0a5f9529cf4b06e6e658d4402fa953ae
dhershman1 <dustinh17@gmail.com>
2019-01-16 13:13:40 -0500

    Small tweaks to export script for future planning

--- scripts/create-export.js
+++ scripts/create-export.js
@@ -3,15 +3,20 @@ const path = require('path')
 const globby = require('globby')
 
 const files = globby.sync(['src/**/*.js', '!src/index.js', '!src/common.js', '!src/_internals'])
-const buildRes = n =>
+const buildRes = type =>
   files.map(f => {
     const { dir, base, name } = path.parse(f)
+    const src = `./${dir.replace('src/', '')}/${base}`
 
-    return `export { default as ${name} } from '${n}/${dir.replace('src/', '')}/${base}'`
+    if (type === 'standard') {
+      return `export { default as ${name} } from '${src}'`
+    }
+
+    return `  ${name}: require('${src}'),`
   }).join('\n')
 
 // Build Exports
-fs.writeFileSync('src/index.js', `${buildRes('.')}\n`)
-// fs.writeFileSync('dist/index.mjs', `${buildRes('../src')}\n`)
+// fs.writeFileSync('src/index.js', `module.exports = {\n${buildRes('cjs')}\n}`)
+fs.writeFileSync('src/index.js', `${buildRes('standard')}\n`)
 
 console.log('Finished Writing Exports...')
