f5c40d46fcdbae6d4ffeb76363968f2e20efd991
dhershman1 <dustinh17@gmail.com>
2019-02-14 11:23:51 -0500

    Created product function

---
+++ tests/number/product.js
@@ -0,0 +1,8 @@
+import test from 'tape'
+import product from '../../src/number/product'
+
+test('product -- Combines array', t => {
+  t.same(product([1, 2, 3]), 6)
+  t.same(product([2, 3, 0]), 0)
+  t.end()
+})
