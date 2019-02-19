03c8acfb5a64c21d335085632dcbb27b7e8e418d
dhershman1 <dustinh17@gmail.com>
2019-02-19 12:12:52 -0500

    Added jsdocs onto median

--- src/number/median.js
+++ src/number/median.js
@@ -4,6 +4,22 @@ import mean from './mean'
 import slice from '../list/slice'
 import pipe from '../function/pipe'
 
+/**
+ * @name median
+ * @function
+ * @since v0.12.0
+ * @category Number
+ * @sig [Number] -> Number
+ * @description Takes an array of numbers and finds the median
+ * @param {Array} list The array of numbers to calculate
+ * @return {Number} The calculated median from the array
+ *
+ * @example
+ * import { median } from 'kyanite'
+ *
+ * median([3, 13, 7, 5, 21, 23, 39, 23, 40, 23, 14, 12, 56, 23, 29]) // => 23
+ * median([3, 13, 7, 5, 21, 23, 23, 40, 23, 14, 12, 56, 23, 29]) // => 22
+ */
 const median = list => {
   const len = list.length
 
