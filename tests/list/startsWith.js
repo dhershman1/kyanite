eaf4cbb2c965a7548ccd73f0af1cba5edc9ebc08
dhershman1 <dhershman@budgetdumpster.com>
2019-07-08 16:38:02 -0400

    Added startsWith function

---
+++ tests/list/startsWith.js
@@ -0,0 +1,18 @@
+import startsWith from '../../src/list/startsWith'
+import test from 'tape'
+
+test('startsWith -- Basic Usage on lists', t => {
+  t.true(startsWith('c', 'cab'))
+  t.true(startsWith(['a'], ['a', 'b', 'c']))
+  t.false(startsWith('b', 'abc'))
+  t.end()
+})
+
+test('startsWith -- Is curried', t => {
+  const fn = startsWith('c')
+
+  t.true(fn('cab'))
+  t.true(fn('c'))
+  t.false(fn('a'))
+  t.end()
+})
