9dba3bea581b4cf603bb5dcc47e5540c2b062a74
dhershman1 <dhershman@budgetdumpster.com>
2020-02-27 14:44:41 -0500

    feat(tail): added new tail function

---
+++ tests/list/tail.js
@@ -0,0 +1,8 @@
+import test from 'tape'
+import tail from '../../src/list/tail'
+
+test('tail() Basics', t => {
+  t.same(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5], 'Gives back tail end of array')
+  t.same(tail('wow'), ['o', 'w'], 'Works properly with strings')
+  t.end()
+})
