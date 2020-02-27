22a300d6861fa5d86dcd7c6cbe345519809b9d67
dhershman1 <dhershman@budgetdumpster.com>
2020-02-27 09:42:43 -0500

    feat(functions): added T and F Functions
    Added T and F functions which return true and false respectively.

---
+++ src/function/T.js
@@ -0,0 +1,18 @@
+
+/**
+ * @name T
+ * @function
+ * @since v1.2.0
+ * @category Function
+ * @sig () -> Boolean
+ * @description
+ * A Function that will always return true, any given parameters are ignored
+ * @return {Boolean} A boolean of true
+ * @example
+ * import { T } from 'kyanite'
+ *
+ * T() // => true
+ */
+const T = () => true
+
+export default T
---
+++ tests/function/T.js
@@ -0,0 +1,8 @@
+import test from 'tape'
+import T from '../../src/function/T'
+
+test('T Basic Tests', t => {
+  t.same(T(), true, 'Returns true')
+  t.same(T(1, 2), true, 'Ignore Parameters')
+  t.end()
+})
