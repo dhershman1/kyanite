8c4a4fb01f643f9b1950431d0403a1b928666d33
dhershman1 <dhershman@budgetdumpster.com>
2020-02-28 09:26:46 -0500

    feat(xor): added new xor function

---
+++ src/function/xor.js
@@ -0,0 +1,25 @@
+import _curry2 from '../_internals/_curry2'
+
+/**
+ * @name xor
+ * @function
+ * @since v1.3.0
+ * @category Function
+ * @sig a -> b -> Boolean
+ * @description
+ * Exclusive or logical operation, returns true if one of the arguments is truthy and the other is falsy, otherwise it returns false
+ * @param {Any} a The first value to check for a truthy value
+ * @param {Any} b The second value to check for a truthy value
+ * @return {Boolean} The boolean outcome of the check
+ *
+ * @example
+ * import { xor } from 'kyanite'
+ *
+ * xor(true, true) // => false
+ * xor(true, false) // => true
+ * xor(false, true) // => true
+ * xor(false, false) // => false
+ */
+const xor = (a, b) => Boolean(!a ^ !b)
+
+export default _curry2(xor)
