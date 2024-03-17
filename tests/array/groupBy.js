f3920e4e844b3e0284fc8556939d173465ed395d
dhershman1 <dustinh17@gmail.com>
2024-03-17 19:19:51 -0400

    Updated: Expanded a bunch of unit tests

--- tests/array/groupBy.js
+++ tests/array/groupBy.js
@@ -4,7 +4,7 @@ import test from 'tape'
 test('groupBy -- Grouped similar number values', t => {
   const results = groupBy(Math.floor, [4.2, 6.1, 6.4])
 
-  t.same(results, { '4': [4.2], '6': [6.1, 6.4] })
+  t.same(results, { 4: [4.2], 6: [6.1, 6.4] })
   t.end()
 })
 
@@ -19,6 +19,45 @@ test('groupBy -- It is curried', t => {
   const g = groupBy(Math.floor)
   const results = g([4.2, 6.1, 6.4])
 
-  t.same(results, { '4': [4.2], '6': [6.1, 6.4] })
+  t.same(results, { 4: [4.2], 6: [6.1, 6.4] })
+  t.end()
+})
+
+test('groupBy -- Returns empty object when given empty list', t => {
+  t.same(groupBy(Math.floor, []), {})
+  t.end()
+})
+
+test('groupBy -- Complex grouping', t => {
+  function grade (score) {
+    return (score < 65) ? 'F' : (score < 70) ? 'D' : (score < 80) ? 'C' : (score < 90) ? 'B' : 'A'
+  }
+
+  function byGrade (student) {
+    return grade(student.score || 0)
+  }
+  const students = [
+    { name: 'Abby', score: 84 },
+    { name: 'Brad', score: 73 },
+    { name: 'Chris', score: 89 },
+    { name: 'Dianne', score: 99 },
+    { name: 'Eddy', score: 58 },
+    { name: 'Fred', score: 67 },
+    { name: 'Gillian', score: 91 },
+    { name: 'Hannah', score: 78 },
+    { name: 'Irene', score: 85 },
+    { name: 'Jack', score: 69 }
+  ]
+
+  t.same(
+    groupBy(byGrade, students),
+    {
+      A: [{ name: 'Dianne', score: 99 }, { name: 'Gillian', score: 91 }],
+      B: [{ name: 'Abby', score: 84 }, { name: 'Chris', score: 89 }, { name: 'Irene', score: 85 }],
+      C: [{ name: 'Brad', score: 73 }, { name: 'Hannah', score: 78 }],
+      D: [{ name: 'Fred', score: 67 }, { name: 'Jack', score: 69 }],
+      F: [{ name: 'Eddy', score: 58 }]
+    }
+  )
   t.end()
 })
