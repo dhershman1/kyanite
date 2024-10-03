e6afca32ea10597ddd8b57d04fca10687d29a613
dhershman1 <dustinh17@gmail.com>
2024-10-02 23:58:28 -0400

    Added: Another pipeP test

--- tests/function/pipeP.js
+++ tests/function/pipeP.js
@@ -11,6 +11,16 @@ test('pipeP -- Basic functionality', t => {
   })
 })
 
+test('pipeP -- Handles async functions', t => {
+  const asyncAdd = a => new Promise(resolve => resolve(a + 1))
+  const asyncMult = a => new Promise(resolve => resolve(a * 2))
+
+  pipeP([asyncAdd, asyncMult], 10).then(d => {
+    t.is(d, 22)
+    t.end()
+  })
+})
+
 test('pipeP -- Is curried', t => {
   const fn = pipeP([foo, bar])
 
