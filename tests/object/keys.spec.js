cba3d9de48393f22930fdcdc6598f7e492e42bac
dhershman1 <dustinh17@gmail.com>
2024-10-13 19:28:19 -0400

    Updated: Expanded upon keys tests

--- tests/object/keys.spec.js
+++ tests/object/keys.spec.js
@@ -1,6 +1,8 @@
 import test from 'tape'
-
 import keys from '../../src/object/keys.js'
+
+// Mock Object.keys to test the fallback logic
+const originalObjectKeys = Object.keys
 
 test('keys -- Basic usage', t => {
   const obj = { a: 1, b: 2, c: 3, d: { x: 100, y: 200 } }
@@ -10,7 +12,7 @@ test('keys -- Basic usage', t => {
   t.end()
 })
 
-test('keys -- Works with primitives', t => {
+test('keys -- Non-object values', t => {
   t.same(keys(null), [])
   t.same(keys(undefined), [])
   t.same(keys(55), [])
@@ -41,5 +43,41 @@ test('keys -- Should not pick up prototypes', t => {
   const cobj = new C()
 
   t.same(keys(cobj), ['a', 'b'])
+  t.end()
+})
+
+test('keys -- Object with no own properties', t => {
+  const obj = Object.create({ a: 1 })
+  t.same(keys(obj), [])
+  t.end()
+})
+
+test('keys -- Fallback logic when Object.keys is not available', t => {
+  Object.keys = undefined
+
+  const obj = { a: 1, b: 2, c: 3 }
+  t.same(keys(obj), ['a', 'b', 'c'])
+
+  Object.keys = originalObjectKeys
+  t.end()
+})
+
+test('keys -- Fallback logic with no own properties', t => {
+  Object.keys = undefined
+
+  const obj = Object.create({ a: 1 })
+  t.same(keys(obj), [])
+
+  Object.keys = originalObjectKeys
+  t.end()
+})
+
+test('keys -- Test NaN not equal to NaN', t => {
+  Object.keys = undefined
+
+  const obj = NaN
+  t.same(keys(obj), [])
+
+  Object.keys = originalObjectKeys
   t.end()
 })
