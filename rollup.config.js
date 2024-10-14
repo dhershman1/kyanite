9b82983f8dcd30ae23f92b014cbdd05fe4e9e011
dhershman1 <dustinh17@gmail.com>
2024-10-13 20:44:27 -0400

    Removed: Bundle analyzer as it isn't working

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,6 @@
-import 'dotenv/config'
 import babel from '@rollup/plugin-babel'
 import filesize from 'rollup-plugin-filesize'
 import terser from '@rollup/plugin-terser'
-import { codecovRollupPlugin } from '@codecov/rollup-plugin'
 
 export default [
   {
@@ -12,11 +10,6 @@ export default [
       terser(),
       filesize({
         showMinifiedSize: false
-      }),
-      codecovRollupPlugin({
-        enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
-        bundleName: 'kyanite',
-        uploadToken: process.env.CODECOV_TOKEN
       })
     ],
     output: {
